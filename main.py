15098c9bb6181fa2ab3a17b9163d7e2278660f9e
mattchoopower <folsom+mdiamond@thecoderschool.com>
2026-02-27 01:36:08 +0000

    we did a guessing game and it was very fun

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import random
-# max=906761326283830153
-max=10
-print (f"guess a number 1 to {max}")
+max = 100
+print(f"guess a number 1 to {max}")
 answer = random.randint(1,max)
 guess = 0
 while guess != answer:
-    guess=int(input())
+    guess = input()
+    try:
+        guess=int(guess)
+    except:
+        print("not a NUMBER go back to ur mommy")
+        continue
+    if guess == answer:
+        print("thats correct, nice job!")
+    if guess < answer:
+        print("too low, even slef is better than u!")
+    if guess > answer:
+        print("TOO HIGH!")
